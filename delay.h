aa8c7dce6de099ab4b4cf0efdf986f05c9bbfc3d
ssalcedo13 <155481902+ssalcedo13@users.noreply.github.com>
2026-05-27 08:46:31 -0700

    Create delay.h

---
+++ delay.h
@@ -0,0 +1,17 @@
+/*
+ * delay.h
+ *
+ *  Created on: Apr 22, 2026
+ *      Author: sdima
+ */
+
+#ifndef INC_DELAY_H_
+#define INC_DELAY_H_
+
+#include "main.h"
+
+void SysTick_Init( void );
+void delay_us (const uint32_t time_us);
+
+
+#endif /* INC_DELAY_H_ */
